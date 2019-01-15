721261edb4a350e4bfbde6fbfc36667b974e5c45
archis99 <archismansahoo8@gmail.com>
2019-01-15 22:16:10 +0530

    Adding spinners

--- client/Components/Search.js
+++ client/Components/Search.js
@@ -6,6 +6,7 @@ class Search extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
+            spinners: false,
             weatherComponent: false,
             searchValue: '',
             place: null,
@@ -19,6 +20,10 @@ class Search extends React.Component {
 
     fetchWeather(e) {
         e.preventDefault();
+        this.setState({
+            spinners: true,
+            weatherComponent: false
+        });
         fetch(`/fetchWeatherDetails?address=${this.state.searchValue}`)
         .then((response) => {
             response.json().then((data) => {
@@ -29,7 +34,8 @@ class Search extends React.Component {
                     feelsLike: data["Feels Like(deg.C)"],
                     humidity: data.Humidity,
                     windSpeed: data["Wind Speed(m/s)"],
-                    currCondition: data["Current Condition"]
+                    currCondition: data["Current Condition"],
+                    spinners: false
                 });
             });
         })
@@ -67,6 +73,18 @@ class Search extends React.Component {
                 </div>
 
                 <div className="row justify-content-center">
+                    {this.state.spinners?
+                    <div>
+                        <div class="spinner-grow text-success" role="status">
+                        <span class="sr-only">Loading...</span>
+                            </div>
+                        <div class="spinner-grow text-danger" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                        <div class="spinner-grow text-warning" role="status">
+                            <span class="sr-only">Loading...</span>
+                        </div>
+                    </div>:null}
                     {this.state.weatherComponent?
                     <WeatherDetail 
                         place={this.state.place}
